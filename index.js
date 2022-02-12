438a46dceb63ebce458b22ec3bc8ff0b58b6ee4d
Daniel Owens <owensdaniel8@yahoo.com>
2022-02-11 21:39:58 -0800

    commit tree e973aac5960bad55f25a56ab8ba7324010f1eaf9

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-// Code your solutions in this file
+
+const names = ["Charlie", "Samip", "Ali"];
+const event = ["birthday"];
+let newNames = [];
+
+function writeCards(names, event) {
+    for (let i = 0; i < names.length; i++) {
+        newNames.push(`Thank you, ${names[i]}, for the wonderful ${event} gift!`);
+    }
+    return newNames;
+}
+
+function countDown() {
+    let number = 10;
+    while (number >= 0) {
+        console.log(number--);
+    }
+}
